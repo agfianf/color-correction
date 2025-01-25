2458ce5220a64aa48109803a27ede2cee5b0a5c4
Agfian (Boy) <muhammadagfian96@gmail.com>
2025-01-25 10:37:03 +0700

    feat: add color checker reference and enhance YOLOv8 detection with patch extraction

--- src/color_correction_asdfghjkl/schemas/yolov8_det.py
+++ src/color_correction_asdfghjkl/schemas/yolov8_det.py
@@ -1,6 +1,15 @@
 import numpy as np
 from pydantic import BaseModel
 
+from color_correction_asdfghjkl.utils.geometry_processing import (
+    extract_intersecting_patches,
+    generate_expected_patches,
+    suggest_missing_patch_coordinates,
+)
+from color_correction_asdfghjkl.utils.image_processing import (
+    calculate_mean_rgb,
+    crop_region_with_margin,
+)
 from color_correction_asdfghjkl.utils.yolo_utils import draw_detections
 
 box_tuple = tuple[int, int, int, int]
@@ -41,3 +50,42 @@ class DetectionResult(BaseModel):
             class_ids=self.class_ids,
             mask_alpha=mask_alpha,
         )
+
+    def get_list_patches(self, input_image: np.ndarray) -> list[np.ndarray]:
+        ls_cards, ls_patches = self.get_each_class_box()
+
+        if len(ls_cards) == 0:
+            raise ValueError("No cards detected")
+
+        if len(ls_patches) == 0:
+            raise ValueError("No patches detected")
+
+        # Extract card coordinates
+        card_box = ls_cards[0]
+        ls_grid_card = generate_expected_patches(card_box)
+
+        # get ls grid card
+        ls_ordered_patch_bbox = extract_intersecting_patches(
+            ls_patches=ls_patches,
+            ls_grid_card=ls_grid_card,
+        )
+
+        if None in ls_ordered_patch_bbox:
+            # Auto fill missing patches ----------------
+            print("Auto fill missing patch...", ls_ordered_patch_bbox)
+            d_suggest = suggest_missing_patch_coordinates(ls_ordered_patch_bbox)
+            for idx, patch in d_suggest.items():
+                ls_ordered_patch_bbox[idx] = patch
+            print(f"result len = {len(ls_ordered_patch_bbox)}")
+
+        ls_rgb_mean_patch = []
+        for coord_patch in ls_ordered_patch_bbox:
+            cropped_patch = crop_region_with_margin(
+                image=input_image,
+                coordinates=coord_patch,
+                margin_ratio=0.2,
+            )
+            rgb_mean_patch = calculate_mean_rgb(cropped_patch)
+            ls_rgb_mean_patch.append(rgb_mean_patch)
+
+        return ls_rgb_mean_patch
