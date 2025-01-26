6f0fab4890739d5afb5985c5400df6cd80af1aec
Agfian (Boy) <muhammadagfian96@gmail.com>
2025-01-26 19:13:31 +0700

    feat: update versioning scheme and enhance project metadata for clarity

--- color_correction_asdfghjkl/__init__.py
+++ color_correction_asdfghjkl/__init__.py
@@ -1,21 +1,12 @@
-__version__ = "0.0.1-alpha"
+__version__ = "0.0.1a1"
 
+# fmt: off
+from .constant.color_checker import reference_color_d50 as REFERENCE_COLOR_D50  # noqa: N812, I001
+from .core.card_detection.yolov8_det_onnx import YOLOv8CardDetector
+from .schemas.yolov8_det import DetectionResult as YOLOv8DetectionResult
+from .services.color_correction import ColorCorrection
 
-from color_correction_asdfghjkl.constant.color_checker import (
-    reference_color_d50 as REFERENCE_COLOR_D50,  # noqa: N812
-)
-from color_correction_asdfghjkl.core.card_detection.yolov8_det_onnx import (
-    YOLOv8CardDetector,
-)
-from color_correction_asdfghjkl.schemas.yolov8_det import (
-    DetectionResult as YOLOv8DetectionResult,
-)
-from color_correction_asdfghjkl.services.color_correction import ColorCorrection
-from color_correction_asdfghjkl.utils.image_processing import (
-    calculate_mean_rgb,
-    crop_region_with_margin,
-    generate_image_patches,
-)
+# fmt: on
 
 __all__ = [
     "__version__",
