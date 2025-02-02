e673f3866f636a8f8b756787e8aabc5c791a840b
Agfian (Boy) <muhammadagfian96@gmail.com>
2025-02-02 16:00:01 +0700

    refactor: streamline preprocessing and postprocessing functions for grid image patches

--- color_correction_asdfghjkl/utils/correction.py
+++ color_correction_asdfghjkl/utils/correction.py
@@ -2,10 +2,8 @@ import numpy as np
 
 
 def preprocessing_compute(input_image: np.ndarray) -> np.ndarray:
-    # Input adalah array (N,3) dari nilai warna patches
-
-    print("pre-processing", input_image.shape)
-    if input_image.shape == (24, 3):  # Khusus untuk ColorChecker 24 patches
+    if input_image.shape == (24, 3):
+        # to handle grid image patches only
         image = input_image.astype(np.float32)
     else:
         image = input_image.reshape(-1, 3).astype(np.float32)
@@ -17,11 +15,10 @@ def postprocessing_compute(
     predict_image: np.ndarray,
 ) -> np.ndarray:
     if len(original_shape) == 2:
+        # to handle grid image patches only
         corrected_image = np.clip(predict_image, 0, 255).astype(np.uint8)
-        print("post-processing", corrected_image.shape, original_shape)
     else:
         h, w, c = original_shape
-        # Clip dan convert kembali ke uint8
         corrected_image = (
             np.clip(predict_image, 0, 255).astype(np.uint8).reshape(h, w, c)
         )
